e6d3ca534d28bd6864e4fb40ebd5bdc4713a5ac1
vajeer81 <vajeermohammad81@gmail.com>
2023-03-02 22:45:23 +0530

    first commit

--- backend/controllers/Deshbordcontrollers.js
+++ backend/controllers/Deshbordcontrollers.js
@@ -5,21 +5,21 @@ const allapi = async (req, res) => {
 try {
   let prodects = await prodect.find({})
   let student = await stdudents.find({})
-  let employ = await Employ.find({})
+  let employes = await Employ.find({})
     arr = [
-      employ = {
+      employes = {
         image:"https://wallpaperaccess.com/full/4322025.jpg",
-        name:"emaploy",
-        data:employ
+        name:"EMPLOYES DETAILS",
+        data:employes
       },
   student={
          image:"https://watermark.lovepik.com/photo/20211130/large/lovepik-primary-school-students-study-picture_501212451.jpg",
-         name:"student",
+         name:"STUDENTS DETAILS",
          data: student
       },
       prodects={
         image:"https://stickybranding.com/wp-content/uploads/2019/11/How-to-name-your-product.jpg",
-         name:"product",
+         name:"PRODUCTS DETAILS",
          data: prodects
       }
 
